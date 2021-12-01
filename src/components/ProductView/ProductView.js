4f41567681806482ec018353420257befc107f15
your name here <mmakhambetov@yandex.ru>
2021-12-01 14:09:36 +0400

    Toggle side panel

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
@@ -6,7 +6,7 @@ import './ProductView.css'
 function ProductView({ products }) {
 
     // TODO: Replace with state variable
-    const sideOpen = true;
+    const [sideOpen, setSideOpen] = useState(true);
 
     return (
         <div className="product-view">
@@ -25,7 +25,7 @@ function ProductView({ products }) {
             <div className="product-side-panel">
                 <div className="product-side-panel-toggle-wrapper">
                     <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
+                         onClick={() => setSideOpen(!sideOpen)}>
                         {sideOpen ? '>' : '<'}
                     </div>
                 </div>
